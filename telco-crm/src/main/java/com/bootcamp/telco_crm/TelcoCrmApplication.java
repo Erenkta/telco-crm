0329f7d10324f6c6cb07d0cef2d5dc3c79dd93da
Eren Nokta <109761393+Erenkta@users.noreply.github.com>
2026-08-16 20:41:40 +0300

    feat : telco-crm project initialized
    telco-crm projesi oluşturuldu ve maven clean install ile doğrulandı.

---
+++ telco-crm/src/main/java/com/bootcamp/telco_crm/TelcoCrmApplication.java
@@ -0,0 +1,13 @@
+package com.bootcamp.telco_crm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TelcoCrmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TelcoCrmApplication.class, args);
+	}
+
+}
